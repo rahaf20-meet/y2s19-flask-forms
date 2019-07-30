854dd1c4ea22cff276cf96ecc1bd5ae328e3272c
myname21-meet <myname21@meet.mit.edu>
2019-07-30 11:29:11 -0400

    forms

--- app.py
+++ app.py
@@ -11,6 +11,21 @@ def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
 #Create an '/add' route here:
+@app.route('/add', methods=['GET', 'POST'])
+def add_student_route():
+	if request.method == 'GET':
+		return render_template('add.html')
+	else:
+		print("Recieved POST request!")
+		name = request.form['student_name']
+		year = request.form['student_year']
+		add_student(name,year)
+		return render_template('add.html')
+
+@app.route('/delete/<int:student_id>', methods=['POST'])
+	delete_student_id(student_id)
+	redirect(url_for(*where you want to go*))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
